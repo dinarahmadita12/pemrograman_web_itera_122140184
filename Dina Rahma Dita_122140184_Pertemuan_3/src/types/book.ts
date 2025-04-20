7aae789dbb624073364d7650403363dbf50b88c8
Dina Rahma Dita <145269715+dinarahmadita12@users.noreply.github.com>
2025-04-20 11:17:40 +0700

    y

--- Dina Rahma Dita_122140184_Pertemuan_3/src/types/book.ts
+++
@@ -1,9 +0,0 @@
-export type BookStatus = 'owned' | 'reading' | 'wishlist';
-
-export interface Book {
-  id: string;
-  title: string;
-  author: string;
-  status: BookStatus;
-  createdAt: string;
-}
